eeb1d95b7a72978f8d302e1db3cacdef307f0296
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2020-09-11 11:09:00 +0300

    Javascript

--- webroot/script.js
+++ webroot/script.js
@@ -2,15 +2,18 @@
 
 
 function changeimg (url, e) {
+  document.getElementById('rock').style.opacity = 1;
   document.getElementById("big__img").src = url;
   let imageList = document.getElementById("image__list");
   let img_child = imageList.children;
   for (i = 0; i < img_child.length; i++) {
     img_child[i].classList.remove('shown');
     e.classList.add('shown');
-  
+
 
   }
+
+
 
 }
 
