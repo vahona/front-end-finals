a1530de5dbe67a011fc80acfbd4a32341468eed3
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2020-09-11 10:00:32 +0300

    Putting some id in the html

--- webroot/script.js
+++ webroot/script.js
@@ -1,14 +1,22 @@
-console.log('we')
+// Function to generate the image everytime the user click the image they want to see in a big size"
+
 
 function changeimg (url, e) {
   document.getElementById("big__img").src = url;
   let imageList = document.getElementById("image__list");
   let img_child = imageList.children;
   for (i = 0; i < img_child.length; i++) {
-    img_child[i].classList.remove('rock');
-    e.classList.add('rock');
+    img_child[i].classList.remove('shown');
+    e.classList.add('shown');
+  
 
   }
 
-  console.log(img_child);
 }
+
+
+
+
+
+
+
